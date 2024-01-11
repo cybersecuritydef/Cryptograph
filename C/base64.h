861677e85d1a63ecf07f9984bd5fe22da5ec9730
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-01-11 17:02:40 +0300

    Update base64.h

--- C/base64.h
+++ C/base64.h
@@ -2,8 +2,8 @@
 #define __BASE64_H__
 
 
-void encode(const char *plain, size_t plain_len, char *encoded, size_t encoded_len);
+void b64encode(const char *plain, size_t plain_len, char *encoded, size_t encoded_len);
 
-void decode(const char *encoded, size_t encoded_len, char *plain, size_t plain_len);
+void b64decode(const char *encoded, size_t encoded_len, char *plain, size_t plain_len);
 
 #endif
