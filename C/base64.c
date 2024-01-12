8dbc028c86ba919a894191f73378747abba93200
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-01-12 13:14:24 +0300

    Update base64.c

--- C/base64.c
+++ C/base64.c
@@ -7,16 +7,18 @@ static const char TABLE[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy
 void b64encode(const unsigned char *inbuf, const size_t inlen, char *outbuf, const size_t outlen){
     size_t index = 0;
     size_t pos = 0;
+    const unsigned char *buf = NULL;
     if(inbuf != NULL && outbuf != NULL && outlen > inlen){
+        buf = (unsigned char *)inbuf;
         while(pos < inlen){
-            outbuf[index++] = TABLE[(inbuf[pos] >> 2)];
-            outbuf[index++] = TABLE[(((inbuf[pos] & 3) << 4) | (inbuf[pos + 1] >> 4))];
+            outbuf[index++] = TABLE[(buf[pos] >> 2)];
+            outbuf[index++] = TABLE[(((buf[pos] & 3) << 4) | (buf[pos + 1] >> 4))];
             if((inlen - pos) > 1)
-                outbuf[index++] = TABLE[(((inbuf[pos + 1] & 15) << 2) | (inbuf[pos + 2] >> 6))];
+                outbuf[index++] = TABLE[(((buf[pos + 1] & 15) << 2) | (buf[pos + 2] >> 6))];
             else
                 outbuf[index++] = '=';
             if((inlen - pos) > 2)
-                outbuf[index++] = TABLE[(inbuf[pos + 2] & 63)];
+                outbuf[index++] = TABLE[(buf[pos + 2] & 63)];
             else
                 outbuf[index++] = '=';
             pos += 3;
@@ -25,18 +27,20 @@ void b64encode(const unsigned char *inbuf, const size_t inlen, char *outbuf, con
     }
 }
 
-void b64decode(const char *inbuf, const size_t inlen, unsigned char *outbuf, const size_t outlen){
+void b64decode(const char *inbuf, const size_t inlen, void *outbuf, const size_t outlen){
     size_t pos = 0;
     size_t index = 0;
+    unsigned char *buf = NULL;
     if(inbuf != NULL && outbuf != NULL && outlen > inlen){
+        buf = (unsigned char *)outbuf;
         while(pos < inlen){
-            outbuf[index++] = ((int)(strchr(TABLE, inbuf[pos]) - TABLE) << 2) | ((int)(strchr(TABLE, inbuf[pos + 1]) - TABLE) >> 4);
+            buf[index++] = ((int)(strchr(TABLE, inbuf[pos]) - TABLE) << 2) | ((int)(strchr(TABLE, inbuf[pos + 1]) - TABLE) >> 4);
             if(inbuf[pos + 2] != '=')
-                outbuf[index++] = (((int)(strchr(TABLE, inbuf[pos + 1]) - TABLE) & 15) << 4) | ((int)(strchr(TABLE, inbuf[pos + 2]) - TABLE) >> 2);
+                buf[index++] = (((int)(strchr(TABLE, inbuf[pos + 1]) - TABLE) & 15) << 4) | ((int)(strchr(TABLE, inbuf[pos + 2]) - TABLE) >> 2);
             if(inbuf[pos + 3] != '=')
-                outbuf[index++] = (((int)(strchr(TABLE, inbuf[pos + 2]) - TABLE) & 3) << 6) | (int)(strchr(TABLE, inbuf[pos + 3]) - TABLE);
+                buf[index++] = (((int)(strchr(TABLE, inbuf[pos + 2]) - TABLE) & 3) << 6) | (int)(strchr(TABLE, inbuf[pos + 3]) - TABLE);
             pos += 4;
         }
-        outbuf[index] = '\0';
+        buf[index] = '\0';
     }
 }
