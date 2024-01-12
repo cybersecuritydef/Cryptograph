50e6b1065a9aa58ed194140c4c71510a052414f0
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-01-12 15:08:41 +0300

    Update base64.h

--- C++/base64.h
+++ C++/base64.h
@@ -11,7 +11,7 @@ public:
         size_t pos = 0;
         std::string outbuf;
         const unsigned char *buf = nullptr;
-        if(inbuf != NULL && inlen > 0){
+        if(inbuf != nullptr && inlen > 0){
             buf = static_cast<const unsigned char *>(inbuf);
             while(pos < inlen){
                 outbuf.push_back(TABLE[(buf[pos] >> 2)]);
@@ -33,8 +33,8 @@ public:
     static void b64encode(const void *inbuf, size_t inlen, char *outbuf, size_t outlen){
         size_t index = 0;
         size_t pos = 0;
-        const unsigned char *buf = NULL;
-        if(inbuf != NULL && outbuf != NULL && outlen > inlen){
+        const unsigned char *buf = nullptr;
+        if(inbuf != nullptr && outbuf != nullptr && inlen > 0 && outlen > inlen){
             buf = (unsigned char *)inbuf;
             while(pos < inlen){
                 outbuf[index++] = TABLE[(buf[pos] >> 2)];
@@ -54,69 +54,73 @@ public:
     }
     
     static void *b64decode(const std::string &inbuf){
-        size_t len = inbuf.size();
+        size_t inlen = inbuf.size();
         size_t pos = 0;
         std::string outbuf;
-        while(pos < len){
-            outbuf.push_back((TABLE[inbuf[pos]] << 2) | (TABLE[inbuf[pos + 1]] >> 4));
-            if(inbuf[pos + 2] != '=')
-                outbuf.push_back(((TABLE[inbuf[pos + 1]] & 15) << 4) | (TABLE[inbuf[pos + 2]] >> 2));
-            if(inbuf[pos + 3] != '=')
-                outbuf.push_back(((TABLE[inbuf[pos + 2]] & 3) << 6) | TABLE[inbuf[pos + 3]]);
-            pos += 4;
-        }
+	if(inlen > 0){
+		while(pos < inlen){
+	            outbuf.push_back((TABLE[inbuf[pos]] << 2) | (TABLE[inbuf[pos + 1]] >> 4));
+	            if(inbuf[pos + 2] != '=')
+	                outbuf.push_back(((TABLE[inbuf[pos + 1]] & 15) << 4) | (TABLE[inbuf[pos + 2]] >> 2));
+	            if(inbuf[pos + 3] != '=')
+	                outbuf.push_back(((TABLE[inbuf[pos + 2]] & 3) << 6) | TABLE[inbuf[pos + 3]]);
+	            pos += 4;
+	        }
+	}        
         return static_cast<void *>(const_cast<char *>(outbuf.c_str()));
     }
     
     static void *b64decode(const char *inbuf, const size_t inlen){
         size_t pos = 0;
         std::string outbuf;
-        while(pos < inlen){
-            outbuf.push_back((TABLE[inbuf[pos]] << 2) | (TABLE[inbuf[pos + 1]] >> 4));
-            if(inbuf[pos + 2] != '=')
-                outbuf.push_back(((TABLE[inbuf[pos + 1]] & 15) << 4) | (TABLE[inbuf[pos + 2]] >> 2));
-            if(inbuf[pos + 3] != '=')
-                outbuf.push_back(((TABLE[inbuf[pos + 2]] & 3) << 6) | TABLE[inbuf[pos + 3]]);
-            pos += 4;
-        }
-        return static_cast<void *>(const_cast<char *>(outbuf.c_str()));
+	if(inbuf != nullptr && inlen > 0){
+		while(pos < inlen){
+	            outbuf.push_back((TABLE[inbuf[pos]] << 2) | (TABLE[inbuf[pos + 1]] >> 4));
+	            if(inbuf[pos + 2] != '=')
+	                outbuf.push_back(((TABLE[inbuf[pos + 1]] & 15) << 4) | (TABLE[inbuf[pos + 2]] >> 2));
+	            if(inbuf[pos + 3] != '=')
+	                outbuf.push_back(((TABLE[inbuf[pos + 2]] & 3) << 6) | TABLE[inbuf[pos + 3]]);
+	            pos += 4;
+		}	        
+	}
+	return static_cast<void *>(const_cast<char *>(outbuf.c_str()));
     }
 	
     static void b64decode(const char *inbuf, const size_t inlen, void *outbuf, const size_t outlen){
-		size_t pos = 0;
-		size_t index = 0;
-		unsigned char *buf = NULL;
-		if(inbuf != NULL && outbuf != NULL && outlen > inlen){
-			buf = (unsigned char *)outbuf;
-			while(pos < inlen){
-				buf[index++] = (TABLE[inbuf[pos]] << 2) | (TABLE[inbuf[pos + 1]] >> 4);
-				if(inbuf[pos + 2] != '=')
-					buf[index++] = ((TABLE[inbuf[pos + 1]] & 15) << 4) | (TABLE[inbuf[pos + 2]] >> 2);
-				if(inbuf[pos + 3] != '=')
-					buf[index++] = ((TABLE[inbuf[pos + 2]] & 3) << 6) | TABLE[inbuf[pos + 3]];
-				pos += 4;
-			}
-			buf[index] = '\0';
+	size_t pos = 0;
+	size_t index = 0;
+	unsigned char *buf = nullptr;
+	if(inbuf != nullptr && outbuf != nullptr && inlen > 0 && outlen > inlen){
+		buf = (unsigned char *)outbuf;
+		while(pos < inlen){
+			buf[index++] = (TABLE[inbuf[pos]] << 2) | (TABLE[inbuf[pos + 1]] >> 4);
+			if(inbuf[pos + 2] != '=')
+				buf[index++] = ((TABLE[inbuf[pos + 1]] & 15) << 4) | (TABLE[inbuf[pos + 2]] >> 2);
+			if(inbuf[pos + 3] != '=')
+				buf[index++] = ((TABLE[inbuf[pos + 2]] & 3) << 6) | TABLE[inbuf[pos + 3]];
+			pos += 4;
 		}
+		buf[index] = '\0';
+	}
     }
 	
     static void b64decode(const std::string &inbuf, void *outbuf, const size_t outlen){
-		size_t inlen = inbuf.size();
-		size_t pos = 0;
-		size_t index = 0;
-		unsigned char *buf = nullptr;
-		if(!inbuf.empty()  && outbuf != nullptr && outlen > inlen){
-			buf = (unsigned char *)outbuf;
-			while(pos < inlen){
-				buf[index++] = (TABLE.find(inbuf[pos]) << 2) | (TABLE.find(inbuf[pos + 1]) >> 4);
-				if(inbuf[pos + 2] != '=')
-					buf[index++] = ((TABLE.find(inbuf[pos + 1]) & 15) << 4) | (TABLE.find(inbuf[pos + 2]) >> 2);
-				if(inbuf[pos + 3] != '=')
-					buf[index++] = ((TABLE.find(inbuf[pos + 2]) & 3) << 6) | TABLE.find(inbuf[pos + 3]);
-				pos += 4;
-			}
-			buf[index] = '\0';
-    }
+	size_t inlen = inbuf.size();
+	size_t pos = 0;
+	size_t index = 0;
+	unsigned char *buf = nullptr;
+	if(outbuf != nullptr && inlen > 0 && outlen > inlen){
+		buf = (unsigned char *)outbuf;
+		while(pos < inlen){
+			buf[index++] = (TABLE.find(inbuf[pos]) << 2) | (TABLE.find(inbuf[pos + 1]) >> 4);
+			if(inbuf[pos + 2] != '=')
+				buf[index++] = ((TABLE.find(inbuf[pos + 1]) & 15) << 4) | (TABLE.find(inbuf[pos + 2]) >> 2);
+			if(inbuf[pos + 3] != '=')
+				buf[index++] = ((TABLE.find(inbuf[pos + 2]) & 3) << 6) | TABLE.find(inbuf[pos + 3]);
+			pos += 4;
+		}
+		buf[index] = '\0';
+    	}
 }
 
 private:
