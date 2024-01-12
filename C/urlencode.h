db2bbe0ff3bb91545f497850e5cf21333fc2557b
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-01-12 13:28:48 +0300

    Update urlencode.h

--- C/urlencode.h
+++ C/urlencode.h
@@ -5,8 +5,8 @@
    size buffer urldecode strlen(string) / 3 + 1
 */
 
-void urlencode(const unsigned char *inbuf, const size_t inlen, char *outbuf, const size_t outlen);
+void urlencode(const void *inbuf, const size_t inlen, char *outbuf, const size_t outlen);
 
-void urldecode(const char *inbuf, const size_t inlen, unsigned char *outbuf, const size_t outlen);
+void urldecode(const char *inbuf, const size_t inlen, void *outbuf, const size_t outlen);
 
 #endif
