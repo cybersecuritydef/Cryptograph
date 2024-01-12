917253b8d93b85f68244750fffdc88ff568112c2
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-01-12 13:22:23 +0300

    Update base16.h

--- C/base16.h
+++ C/base16.h
@@ -5,8 +5,8 @@
    size buffer decode strlen(string) / 2 + 1
 */
 
-void b16encode(const unsigned char *inbuf, const size_t inlen, char *outbuf, const size_t outlen);
+void b16encode(const void *inbuf, const size_t inlen, char *outbuf, const size_t outlen);
 
-void b16decode(const char *inbuf, const size_t inlen, unsigned char *outbuf, const size_t outlen);
+void b16decode(const char *inbuf, const size_t inlen, void *outbuf, const size_t outlen);
 
 #endif
