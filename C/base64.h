e6998a4ed5faf747168d3340f19c46b401f6f7b5
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-01-12 13:19:57 +0300

    Update base64.h

--- C/base64.h
+++ C/base64.h
@@ -4,8 +4,8 @@
 /* size buffer encode  (4 * ((strlen(string) + 2) / 3)) + 1
    size buffer decode (strlen(string) * 3) / 4
 */
-void b64encode(const unsigned char *inbuf, const size_t inlen, char *outbuf, const size_t outlen);
+void b64encode(const void *inbuf, const size_t inlen, char *outbuf, const size_t outlen);
 
-void b64decode(const char *inbuf, const size_t inlen, unsigned char *outbuf, const size_t outlen);
+void b64decode(const char *inbuf, const size_t inlen, void *outbuf, const size_t outlen);
 
 #endif
