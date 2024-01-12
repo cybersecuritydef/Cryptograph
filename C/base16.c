94dd9b79607288b293ff7d1324dffe6a6164d769
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-01-12 19:55:20 +0300

    Update base16.c

--- C/base16.c
+++ C/base16.c
@@ -8,9 +8,9 @@ void b16encode(const void *inbuf, const size_t inlen, char *outbuf, const size_t
     size_t index = 0;
     size_t pos = 0;
     const unsigned char *buf = NULL;
-    if(inbuf != NULL && outbuf != NULL && outlen > inlen){
+    if(inbuf != NULL && outbuf != NULL){
         buf = (unsigned char *)inbuf;
-        while(pos < inlen){
+        while(pos < inlen && index < outlen){
             outbuf[index++] = TABLE[buf[pos] >> 4];
             outbuf[index++] = TABLE[buf[pos++] & 15];
         }
