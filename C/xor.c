73a66a18a003280ac2f9b48e82d8127c52cd58fa
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-01-12 12:51:07 +0300

    Update xor.c

--- C/xor.c
+++ C/xor.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void xor(const char *txt, const char *key, const size_t key_len, char *txt_xor, const size_t len){
+void xor(const unsigned char *inbuf, const size_t inlen, const char *key, const int keylen, unsigned char *outbuf, const int outlen){
     int index = 0;
-    if(txt != NULL && key != NULL && txt_xor != NULL && len >= strlen(txt)){
-        for(index = 0; index < len; index++)
-            txt_xor[index] = txt[index] ^ key[index % key_len];
+    if(inbuf != NULL && key != NULL && outbuf != NULL && inlen > 0 && outlen > 0){
+        for(index = 0; index < inlen; index++){
+            outbuf[index] = inbuf[index] ^ key[index % keylen];
+        }   
     }
 }
