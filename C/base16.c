5384341b6879b5c0db134bf8a6c9ba3687427f19
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-01-12 13:22:00 +0300

    Update base16.c

--- C/base16.c
+++ C/base16.c
@@ -4,26 +4,30 @@
 
 static const char TABLE[] = "0123456789ABCDEF";
 
-void b16encode(const unsigned char *inbuf, const size_t inlen, char *outbuf, const size_t outlen){
+void b16encode(const void *inbuf, const size_t inlen, char *outbuf, const size_t outlen){
     size_t index = 0;
     size_t pos = 0;
+    const unsigned char *buf = NULL;
     if(inbuf != NULL && outbuf != NULL && outlen > inlen){
+        buf = (unsigned char *)inbuf;
         while(pos < inlen){
-            outbuf[index++] = TABLE[inbuf[pos] >> 4];
-            outbuf[index++] = TABLE[inbuf[pos++] & 15];
+            outbuf[index++] = TABLE[buf[pos] >> 4];
+            outbuf[index++] = TABLE[buf[pos++] & 15];
         }
         outbuf[index] = '\0';
     }
 }
 
-void b16decode(const char *inbuf, const size_t inlen, unsigned char *outbuf, const size_t outlen){
+void b16decode(const char *inbuf, const size_t inlen, void *outbuf, const size_t outlen){
     size_t index = 0;
     size_t pos = 0;
+    unsigned char *buf = NULL;
     if(inbuf != NULL && outbuf != NULL && outlen > inlen){
+        buf = (unsigned char *)outbuf;
         while(pos < inlen){
-            outbuf[index++] = ((int)(strchr(TABLE, inbuf[pos]) - TABLE) << 4) | (int)((strchr(TABLE, inbuf[pos + 1])) - TABLE);
+            buf[index++] = ((int)(strchr(TABLE, inbuf[pos]) - TABLE) << 4) | (int)((strchr(TABLE, inbuf[pos + 1])) - TABLE);
             pos += 2;
         }
-        outbuf[index] = '\0';
+        buf[index] = '\0';
     }
 }
