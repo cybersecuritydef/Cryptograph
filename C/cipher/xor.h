08e69d716cc0417754a2f3453da46e7ed847da13
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2026-05-27 01:37:32 +0300

    Update xor.h

--- C/cipher/xor.h
+++ C/cipher/xor.h
@@ -1,6 +1,6 @@
 #ifndef __XOR_H__
 #define __XOR_H__
 
-void xor(const void *inbuf, const size_t inlen, const void *key, const int keylen, void *outbuf);
+void xor(const void *inbuf, const size_t inlen, const void *key, const size_t keylen, void *outbuf);
 
 #endif
